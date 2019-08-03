ff4edec070c41189722dae996ed9719d101dda5e
Danuta <kawecka.d@gmail.com>
2019-08-03 20:25:11 +0200

    H-D1-zad1

--- 3_Django/coderslab/homework/models.py
+++ 3_Django/coderslab/homework/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+VAT = (
+    (0, "0"),
+    (1, "0.05"),
+    (2, "0.08"),
+    (3, "0.23"),
+)
 # Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=64)
+    slug = models.SlugField(max_length=64, unique=True)
+
+class Product(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    vat = models.IntegerField(choices=VAT)
+    stock = models.IntegerField(default=0)
+    categories = models.ManyToManyField(Category)
+
+
